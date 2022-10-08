55ae028fc2faa68fbf955c15e134a818a61ed67f
casgaromio1 <casgaromio@naver.com>
2022-10-08 21:37:59 +0900

    Get command line arguments

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,11 +1,14 @@
 import click
 
 @click.command()
-def cli():
+@click.option("--message", "-m", default="LGTM",
+              show_default=True, help="이미지에 추가할 문자열")
+@click.argument("keyword")
+def cli(keyword, message):
     """LGTM 이미지 생성도구"""
     lgtm()
     click.echo('lgtm') # 동작 확인용
     
-def lgtm():
+def lgtm(keyword, message):
     # 여기에 로직을 추가해 나감
     pass
